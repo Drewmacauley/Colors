386fa2cb03e743fb9e26734f333eb55878ae2702
Drewmacauley <macauleyandy03@gmail.com>
2020-08-11 11:23:23 -0700

    add classes,
    Co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(documemnt).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  }
+});
+
+$("button#yellow").click(fuction() {
+  $("body").addClass("yellow-background");
+});
+
+$("button#red").click(function() {
+  $(:body).addClass("red-background");
+});
+});
